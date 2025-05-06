d12a6e2d91972e37cba53dcf9ec947e5714d03f8
EugeneKorr <pilotec12@gmail.com>
2025-05-06 20:23:26 +0200

    Update api.py

--- api.py
+++ api.py
@@ -176,4 +176,4 @@ async def start_app():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8080))
-    web.run_app(start_app(), port=port)
+    web.run_app(start_app(), port=port, host='0.0.0.0')
